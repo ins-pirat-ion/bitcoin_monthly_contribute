d36c6b359a561dc96f80457e58e4e98ec9dcefa4
Filip Krška <fill_io@centrum.cz>
2013-11-24 20:21:29 +0100

    lynx replaced by curl mtgox ticker replaced by coindesk price index ticker

--- HOME_DOTbitcoin_monthly/bitcoin_monthly_contribute.conf.rb
+++ HOME_DOTbitcoin_monthly/bitcoin_monthly_contribute.conf.rb
@@ -3,11 +3,11 @@ module  BitcoinMonthly
 
   Currency = "CZK"
 
-  Btc_in_usd_ticker_cmd = 'lynx --dump http://data.mtgox.com/api/1/BTCUSD/ticker\
-    |sed \'s/^.*buy":{"value":"//;s/".*$//\''
-  Btc_in_usd = `#{btc_in_usd_ticker_cmd}`
+  Btc_in_usd_ticker_cmd = 'curl https://api.coindesk.com/v1/bpi/currentprice/USD.json\
+    |sed \'s/^.*"rate":"//;s/".*$//\''
+  Btc_in_usd = `#{Btc_in_usd_ticker_cmd}`
 
-  Usd_in_currency_ticker_cmd = "lynx --dump http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt\
+  Usd_in_currency_ticker_cmd = "curl http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt\
     |grep USD|sed 's/^.*|//;s/,/./'"
   Usd_in_currency = `#{Usd_in_currency_ticker_cmd}`
 end
